27a84491a52ef1b95cbd0c828174f389a142f17b
Kevin Canlas <kvncnlsdsgns@gmail.com>
2020-11-27 19:44:06 -0500

    Set up navigation menu and dark overlay.

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 gsap.registerPlugin(ScrollTrigger);
-/* Navbar Animations */
 
 const menuBtn = document.querySelector(".menu");
 const nav = document.querySelector("nav");
 const logo = document.querySelector(".logo");
 const navList = document.querySelector(".nav-list");
+const darkOverlay = document.querySelector(".dark-overlay");
+const switchPrev = document.querySelector(".switch__previous");
+const switchNext = document.querySelector(".switch__next");
 
+/* Navbar Animations Starts */
 menuBtn.addEventListener("click", () => {
   if (!menuBtn.classList.contains("opened")) {
     menuBtn.classList.add("opened");
@@ -21,6 +24,8 @@ function showMenu() {
   logo.style.display = "none";
   navList.style.display = "flex";
   menuBtn.classList.add("menu-x");
+  darkOverlay.style.height = "200vh";
+  darkOverlay.style.width = "100vw";
 }
 
 function closeMenu() {
@@ -28,4 +33,27 @@ function closeMenu() {
   logo.style.display = "block";
   navList.style.display = "none";
   menuBtn.classList.remove("menu-x");
+  darkOverlay.style.height = "0vh";
+  darkOverlay.style.width = "0vw";
 }
+/* Navbar Animations Ends */
+
+switchPrev.addEventListener("click", () => {
+  /* If discover is displayed */
+  /* Show manufactured */
+  /* If manufactured is displayed */
+  /* Show available */
+  /* If available is displayed */
+  /* Show discover */
+});
+
+switchNext.addEventListener("click", () => {
+  /* If discover is displayed */
+  /* Show available */
+  /* If available is displayed */
+  /* Show manufactured */
+  /* If manufactured is displayed */
+  /* Show discover */
+});
+
+/* Next / Previous Buttons */
