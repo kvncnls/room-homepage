a3916be30236ddae64b9c966c68a0e6910957b50
Kevin Canlas <kvncnlsdsgns@gmail.com>
2020-11-27 23:52:55 -0500

    Added copy change on chevron click.

--- app.js
+++ app.js
@@ -46,6 +46,7 @@ const imageDiv = document.querySelector(".main-block__image");
 const image = document.querySelector(".main-block__image img");
 const prevBtn = document.querySelector(".switch__previous");
 const nextBtn = document.querySelector(".switch__next");
+const copy = document.querySelector(".copy");
 
 let imgNo = 1;
 let prefix;
@@ -70,10 +71,35 @@ prevBtn.addEventListener("click", () => {
     prefix = "/images/mobile-image-hero-";
     image.src = `${prefix}${imgNo}.jpg`;
   }
-  /* Create imgNo --> Copy change */
-  /* If 1, dsicover */
-  /* If 2, available */
-  /* If 3, manufacture */
+  /* imgNo --> Copy change */
+  if (imgNo === 1) {
+    copy.innerHTML = `
+    <h1 class="title">
+    Discover innovative ways to decorate
+    </h1>
+    <p class="description">
+    We provide unmatched quality, comfort, and style for property owners across the country.
+    Our experts combine form and function in bringing your vision to life. Create a room in your
+    own style with our collection and make your property a reflection of you and what you love.
+    </p>
+    `;
+  } else if (imgNo === 2) {
+    copy.innerHTML = `
+    <h1 class="title">We are available all across the globe</h1>
+    <p class="description">With stores all over the world, it's easy for you to find furniture for your home or place
+    of business.
+    Locally, we’re in most major cities throughout the country. Find the branch nearest you using our
+    store locator. Any questions? Don't hesitate to contact us today.</p>
+    `;
+  } else if (imgNo === 3) {
+    copy.innerHTML = `
+    <h1 class="title">Manufactured with the best materials</h1>
+    <p class="description">Our modern furniture store provide a high level of quality. Our company has invested in
+    advanced technology
+    to ensure that every product is made as perfect and as consistent as possible. With three decades of
+    experience in this industry, we understand what customers want for their home and office.</p>
+    `;
+  }
 });
 
 nextBtn.addEventListener("click", () => {
@@ -89,8 +115,33 @@ nextBtn.addEventListener("click", () => {
     prefix = "/images/mobile-image-hero-";
     image.src = `${prefix}${imgNo}.jpg`;
   }
-  /* Create imgNo --> Copy change */
-  /* If 1, dsicover */
-  /* If 2, available */
-  /* If 3, manufacture */
+  /* imgNo --> Copy change */
+  if (imgNo === 1) {
+    copy.innerHTML = `
+    <h1 class="title">
+    Discover innovative ways to decorate
+    </h1>
+    <p class="description">
+    We provide unmatched quality, comfort, and style for property owners across the country.
+    Our experts combine form and function in bringing your vision to life. Create a room in your
+    own style with our collection and make your property a reflection of you and what you love.
+    </p>
+    `;
+  } else if (imgNo === 2) {
+    copy.innerHTML = `
+    <h1 class="title">We are available all across the globe</h1>
+    <p class="description">With stores all over the world, it's easy for you to find furniture for your home or place
+    of business.
+    Locally, we’re in most major cities throughout the country. Find the branch nearest you using our
+    store locator. Any questions? Don't hesitate to contact us today.</p>
+    `;
+  } else if (imgNo === 3) {
+    copy.innerHTML = `
+    <h1 class="title">Manufactured with the best materials</h1>
+    <p class="description">Our modern furniture store provide a high level of quality. Our company has invested in
+    advanced technology
+    to ensure that every product is made as perfect and as consistent as possible. With three decades of
+    experience in this industry, we understand what customers want for their home and office.</p>
+    `;
+  }
 });
